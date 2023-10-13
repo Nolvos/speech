b516ef58a810164a02f2216aecf12bebd2ef2877
Nolvos <119397000+Nolvos@users.noreply.github.com>
2023-10-13 22:34:11 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,89 +1,19 @@
-import os
-import librosa
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics.pairwise import euclidean_distances
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from flask import Flask, request, jsonify
+# app.py
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
-# Constants
-DATA_DIR = "/content/drive/MyDrive/Speech_Accent/recordings"
-MAX_FEATURE_LENGTH = 2000  # Maximum length for padding/truncating
 
-# Function to extract MFCC features from audio file
-def extract_features(file_path):
-    audio, _ = librosa.load(file_path, sr=22050)
-    mfccs = librosa.feature.mfcc(y=audio, sr=22050, n_mfcc=13)
+# Sample stories
+stories = [
+    "Once upon a time, there was a cat.",
+    "In a faraway land, a brave knight saved the day.",
+    "A little bird flew across the blue sky."
+]
 
-    # Pad or truncate to make consistent length
-    if mfccs.shape[1] < MAX_FEATURE_LENGTH:
-        pad_width = MAX_FEATURE_LENGTH - mfccs.shape[1]
-        mfccs = np.pad(mfccs, pad_width=((0, 0), (0, pad_width)), mode='constant')
-    else:
-        mfccs = mfccs[:, :MAX_FEATURE_LENGTH]
+# API endpoint to get stories
+@app.route('/api/stories', methods=['GET'])
+def get_stories():
+    return jsonify(stories)
 
-    return mfccs.flatten()
-
-# Load data and extract features
-def load_data():
-    features = []
-    labels = []
-
-    for subdir, _, files in os.walk(DATA_DIR):
-        for file in files:
-            file_path = os.path.join(subdir, file)
-            label = os.path.basename(subdir)  # Use the immediate directory name as the label
-
-            if file.endswith('.mp3'):
-                mfccs = extract_features(file_path)
-                features.append(mfccs)
-                labels.append(label)
-
-    return np.array(features), np.array(labels)
-
-# Load and preprocess data
-features, labels = load_data()
-
-# Ensure features and labels are non-empty
-if len(features) == 0:
-    print("No features extracted. Check data directory and file formats.")
-    exit()
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
-
-# Train a RandomForestClassifier
-clf = RandomForestClassifier(n_estimators=100, random_state=42)
-clf.fit(X_train, y_train)
-
-# Evaluate the model
-y_pred = clf.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print('Accuracy:', accuracy)
-
-# Provide feedback based on accuracy
-if accuracy >= 0.75:
-    print('Excellent')
-elif accuracy >= 0.5:
-    print('Very Good')
-else:
-    print('49% or less. Try again.')
-
-
-@app.route('/api/check_accuracy', methods=['POST'])
-def calculate_accuracy():
-    spoken_text = request.get_json().get('spokenText', '')
-    # Replace this with your accuracy calculation logic
-    accuracy = calculate_accuracy_function(spoken_text)
-    return jsonify({'accuracy': accuracy})
-
-def calculate_accuracy_function(spoken_text):
-    # Implement your accuracy calculation logic here
-    # For demonstration purposes, we're returning a random accuracy between 0 and 1
-    import random
-    return random.random()
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
